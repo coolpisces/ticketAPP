fbc5b8bf8be831c0b1527a95160076d8786ef1fa
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2023-06-21 17:05:56 +0300

    changes

--- js/myScript.js
+++ js/myScript.js
@@ -6,20 +6,26 @@ function sehirSec() {
 }
 
 function sinemaSecim() {
-    let bilet = document.getElementById("bilet");
-    let biletTitle = document.getElementById("bilet-title");
-    let afis = document.getElementById("afis");
+    try {
 
-    let menulinks = document.getElementsByClassName("card");
-    Array.from(menulinks).forEach((menulink) => {
-        menulink.addEventListener("click", () => {
-            document.getElementById("bilet-img").src = menulink.firstElementChild.getAttribute("src");
-            biletTitle.textContent = menulink.lastElementChild.children[0].textContent;
-            bilet.style.display = "flex";
-            afis.style.display = "none";
+
+        let bilet = document.getElementById("bilet");
+        let biletTitle = document.getElementById("bilet-title");
+        let afis = document.getElementById("afis");
+
+        let menulinks = document.getElementsByClassName("card");
+        Array.from(menulinks).forEach((menulink) => {
+            menulink.addEventListener("click", () => {
+                document.getElementById("bilet-img").src = menulink.firstElementChild.getAttribute("src");
+                biletTitle.textContent = menulink.lastElementChild.children[0].textContent;
+                bilet.style.display = "flex";
+                afis.style.display = "none";
+            });
         });
-    });
-
+    }
+    catch (err) {
+        throw err.textContent;
+    }
     /*
     let bilet = document.getElementById("bilet");
     let afis = document.getElementById("afis");
@@ -34,4 +40,23 @@ function sinemaSecim() {
 }
 function kapat() {
     document.getElementById("sehirSec-buton").innerHTML = "Kapat";
+}
+function geriAl() {
+    let bilet = document.getElementById("bilet");
+    let afis = document.getElementById("afis");
+    bilet.style.display = "none";
+    afis.style.display = "flex";
+}
+
+function biletSecimi() {
+    let tutar = 0;
+    const normal = 80;
+    const ogrenci = 60;
+    let koltuks = document.getElementsByClassName("col-1");
+    Array.from(koltuks).forEach((koltuk) => {
+        koltuk.addEventListener("click", () => {
+            koltuk.style.backgroundColor = "green";
+            tutar += normal;
+        });
+    });
 }
