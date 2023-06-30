7008343ae623ff48e86cacacc82c74a643479011
coolpisces <coolpisces22@gmail.com>
2023-06-30 15:15:22 +0300

    finished

--- js/myScript.js
+++ js/myScript.js
@@ -8,13 +8,14 @@ function sehirSec() {
 function sinemaSecim() {
     try {
         if (document.getElementById("sehirButon").classList.contains("pasif")) {
-            alert("Öncelikle bulunduğunuz şehri seçin!")
+            alert("Öncelikle bulunduğunuz şehri seçin!");
         } else {
             document.getElementById("sinemaAdi").textContent = document.getElementById("sehirButon").textContent;
             let bilet = document.getElementById("bilet");
             let biletTitle = document.getElementById("bilet-title");
             let biletTitle2 = document.getElementById("bilet-title2");
             let afis = document.getElementById("afis");
+            var hangiFilm = document.getElementById("bilet-title2");
 
             let menulinks = document.getElementsByClassName("card");
             Array.from(menulinks).forEach((menulink) => {
@@ -23,10 +24,70 @@ function sinemaSecim() {
                     document.getElementById("bilet-img2").src = menulink.firstElementChild.getAttribute("src");
                     biletTitle.textContent = menulink.lastElementChild.children[0].textContent;
                     biletTitle2.textContent = menulink.lastElementChild.children[0].textContent;
+                    hangiFilm.textContent = menulink.lastElementChild.children[0].textContent;
+                    if (hangiFilm.textContent == "Adalet") {
+                        document.getElementById("Adalet").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Özgür Bakar";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Ufuk Özkan";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Çağan Arslan";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Eslem Akar";
+                    }
+                    else if (hangiFilm.textContent == "Fleabag") {
+                        document.getElementById("Fleabag").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Vicky Jones";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Phoebe Bridge";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = " ";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = " ";
+                    }
+                    else if (hangiFilm.textContent == "Joyland") {
+                        document.getElementById("Joyland").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Saim Sadiq";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Ali Junejo";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Rasti Farooq";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Alina Khan";
+                    }
+                    else if (hangiFilm.textContent == "Kerr") {
+                        document.getElementById("Kerr").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Tayfun Pirselimoğlu";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Erdem Şenocak";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Jale Arıkan";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Rıza Akın";
+                    }
+                    else if (hangiFilm.textContent == "Obsesyon") {
+                        document.getElementById("Obsesyon").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Eray Altay";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Ekin Duymaz";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Ege Kökenli";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Burcu Karakaya";
+                    }
+                    else if (hangiFilm.textContent == "Ren Altını") {
+                        document.getElementById("RenAltini").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Fatih Akın";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Emilio Sakraya";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Mona Pirzad";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Julia Goldberg";
+                    }
+                    else if (hangiFilm.textContent == "Suzume") {
+                        document.getElementById("Suzume").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Makoto Shinkai";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Nanoka Hara";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Hokuto Matsumura";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Eri Fukatsu";
+                    }
+                    else if (hangiFilm.textContent == "The Flash") {
+                        document.getElementById("TheFlash").style.display = "inline";
+                        document.getElementById("yonetmen").lastElementChild.textContent = "Andy Muschietti";
+                        document.getElementById("oyuncular").lastElementChild.children[0].textContent = "Ben Affleck";
+                        document.getElementById("oyuncular").lastElementChild.children[1].textContent = "Ezra Miller";
+                        document.getElementById("oyuncular").lastElementChild.children[2].textContent = "Sasha Calle";
+                    }
                     filmDetay.style.display = "inline";
                     afis.style.display = "none";
                 });
             });
+
+
+
         }
 
 
@@ -64,25 +125,23 @@ koltuklar.addEventListener('click', (e) => {
         let total = seciliKoltukSayisi * 80;
         document.getElementById('normalTutar').innerHTML = total.toString() + ",00 TL";
         toplam = Number(total);
-
     }
-
+    if (document.getElementsByClassName("secilen").length != 0) document.getElementById("onayButonu").classList.remove("disabled");
 });
 
 
 
 function secimiOnayla() {
-    if (toplam == 0)
-        alert("Önce bilet seçmelisiniz!"); else {
+
+    if (document.getElementsByClassName('secilen').length != 0) {
         document.getElementById("sectiniz").innerHTML = "";
         let sayi = document.getElementsByClassName('secilen').length;
         for (let k = 0; k < sayi; k++) {
             document.getElementById("sectiniz").innerHTML += document.getElementsByClassName('secilen')[k].textContent + " -";
         }
         document.getElementById("sat").style.visibility = "visible";
-
-
-
+    } else {
+        document.getElementById("onayButonu").classList.add("disabled");
     }
 
 }
@@ -92,8 +151,7 @@ function sat() {
 
     let secilenKoltuklar = document.getElementById("sectiniz").textContent;
 
-
-    alert(secilenKoltuklar + " koltuk numaralı " + document.getElementById('normalTutar').textContent + " tutarındaki biletinizin/biletlerinizin ödemesi başarıyla gerçekleşmiştir! 3 saniye sonra Anasayfaya yönlendirileceksiniz...");
+    document.getElementById("uyari").innerHTML = secilenKoltuklar + " koltuk numaralı " + document.getElementById('normalTutar').textContent + " tutarındaki biletinizin/biletlerinizin ödemesi başarıyla gerçekleşmiştir!";
     document.getElementById("sectiniz").innerHTML = "";
     document.getElementById("sat").style.visibility = "hidden";
     document.getElementById("normalTutar").innerHTML = "00,00 TL";
@@ -104,12 +162,19 @@ function sat() {
         document.getElementsByClassName("secilen")[i].style.cursor = "not-allowed";
     }
 
-    setTimeout("window.location = 'index.html'", 3000);
 
+
+}
+
+function yonlendir() {
+    setTimeout("window.location = 'index.html'", 300);
 }
 function detayToBilet() {
 
     filmDetay.style.display = "none";
     bilet.style.display = "flex";
 }
+
+
+
 
